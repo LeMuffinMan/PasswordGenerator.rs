8670f954a1d90143d102f400bf9d7e716cc92f89
muffin <muffin@arch.man>
2025-08-11 19:53:28 +0200

    basic parsing of config file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-
+use std::fs;
+use std::error::Error;
 
 struct PasswordConfig {
     length: u8,
@@ -8,22 +9,6 @@ struct PasswordConfig {
     symbol: bool,
 }
 
-enum Strength {
-    Weak,
-    Medium,
-    Strong,
-}
-
-fn strength_label(strength: &Strength) -> String {
-    match &strength {
-        Strength::Weak => "The password strength is weak".to_string(),
-        Strength::Medium => "The password strength is medium".to_string(),
-        Strength::Strong => "The password strength is strong".to_string(),
-    }
-
-}
-
-//impl : methode associee a la struct passwordconfig
 impl PasswordConfig {
     fn describe(&self) {
         println!("length = {}", self.length);
@@ -32,14 +17,59 @@ impl PasswordConfig {
         println!("digit = {}", self.digit);
         println!("symbol = {}", self.symbol);
     }
-    //&self : ne modifie pas la struct
-    //&mut self : peut modifier la struct
-    //pas de self : fonction associee / statique ? (fn name() -> Self)
-    //methode qui consomme la struct (fn name (self))
 }
 
-fn main() {
-    let config = PasswordConfig {
+fn get_value_from_line(line: &str, config: &mut PasswordConfig) -> Result<(), Box<dyn Error>> {
+    
+    let line = line.trim();
+
+        if line.is_empty() || line.starts_with('#') {
+            return Ok(());
+        }
+
+        // if let Some((key, value)) est une forme de déstructuration pattern matching.
+        // "Si mon Option (retournee par split_once) est Some((x, y)), mets x dans key et y dans value et exécute le bloc".
+
+        if let Some((key, value)) = line.split_once('=') {
+
+            let key = key.trim();
+            let value = value.trim();
+
+            match key {
+                "length" => {
+                    config.length = value.parse()?
+                }
+                "lowercase" => {
+                    config.lowercase = value.parse()?
+                }
+                "uppercase" => {
+                    config.uppercase = value.parse()?
+                }
+                "digit" => {
+                    config.digit = value.parse()?
+                }
+                "symbol" => {
+                    config.symbol = value.parse()?
+                }
+                _ => {
+                    return Err(format!("Error : unknown key {key}").into());
+                }
+            }
+        } else {
+            return Err(format!("Error : invalid line : {line}").into())
+        }
+    Ok(())
+}
+
+//Synthesis ex0..=3 : 
+//write a program that read in a config file to fill a struct containing the configuration to
+//generate a passwrod 
+//errors must be handled
+//no warning at compile, no panic allowed
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let mut config = PasswordConfig {
         length: 15,
         lowercase: true,
         uppercase: true,
@@ -47,7 +77,14 @@ fn main() {
         symbol: true,
     };
 
+    let path = "config.txt";
+    let content = fs::read_to_string(path)?;
+
+    for line in content.lines() {
+        // println!("{line}");
+        get_value_from_line(line, &mut config)?;
+    }
+
     config.describe();
+    Ok(())
 }
-
-
