b4306af199a20d42a439081f9122669d98e635b2
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-08-12 10:30:04 +0200

    main documented

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ use parse_config::PasswordConfig; //then use to the content
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
+    //PasswordConfig mutable :
+    //- default values 
+    //- modified if a config.txt is found
+    //- modified is arguments are found
     let mut config = PasswordConfig {
         length: 15,
         lowercase: true,
@@ -20,7 +24,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     let path = "config.txt";
-    let content = fs::read_to_string(path)?;
+    let content = fs::read_to_string(path)?; //get all the file in one string "content" with \n
 
     for line in content.lines() {
         // println!("{line}");
@@ -28,16 +32,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     config.describe();
 
+    //declare an instance of random number generator
     let mut _rng = rng();
 
+    //Declaring a new empty String
     let mut password = String::with_capacity(config.length);
 
+    //fill charset with PasswordConfig infos
     let charset = fill_charset(&config)?;
 
     for _ in 0..config.length {
 
         if let Some(&c) = charset.choose(&mut _rng) {
-            password.push(c);
+            password.push(c); //add at the end
         }
     }
     println!("Generated password : {password}");
