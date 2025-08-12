cea28d46de75dee54a4e5990faa1becd3f3809ce
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-08-12 11:23:48 +0200

    q&a answered and division for generation function

---
+++ src/generation.rs
@@ -0,0 +1,24 @@
+use rand::rng; // not in std 
+use rand::prelude::IndexedRandom;
+
+//type &[char] allow to use charset without consuming it ?
+//it's a slice, it's borrowing
+//may i want to consume it once i used it in this function ?
+pub fn generation(charset: &[char], length: usize) -> String {
+
+    //declare an instance of random number generator
+    //not needed using thread_rng ?
+    let mut _rng = rng();
+
+    //Declaring a new empty String
+    let mut password = String::with_capacity(length);
+
+    for _ in 0..length {
+
+        if let Some(&c) = charset.choose(&mut _rng) {
+            password.push(c); //add at the end
+        }
+    }
+    println!("Generated password : {password}");
+    password
+} 
