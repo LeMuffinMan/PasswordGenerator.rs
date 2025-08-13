79f513813b73bf0d28ef414a703de56a52db82ab
muffin <muffin@arch.man>
2025-08-13 12:07:42 +0200

    charset.rs add

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,16 @@
-// use std::fs;
 use clap::Parser;
-// use serde::{Deserialize, Serialize};
 
-mod parse_config;
 mod generation;
-mod cli;
-
 use generation::generation;
 
+mod cli;
 use cli::Cli;
 
-use parse_config::PasswordConfig; //then use to the content
+mod parse_config;
+use parse_config::PasswordConfig;
 
-const LOWERCASE: [char; 26] = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
-    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
-    'u', 'v', 'w', 'x', 'y', 'z',
-];
-
-const UPPERCASE: [char; 26] = [
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
-    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
-    'U', 'V', 'W', 'X', 'Y', 'Z',
-];
-
-const DIGIT: [char; 10] = ['0', '1', '2', '3', '4',
-    '5', '6', '7', '8', '9'];
-
-const SYMBOL: [char; 10] = ['!', '@', '#', '$', '%',
-    '^', '&', '*', '(', ')'];
-
-
-pub fn fill_charset(config: &PasswordConfig)
-    -> Result<Vec<char>, Box<dyn std::error::Error>>
-{
-    let mut charset: Vec<char> = Vec::new();
-    if config.lowercase {
-        charset.extend_from_slice(&LOWERCASE);
-    }
-    if config.uppercase {
-        charset.extend_from_slice(&UPPERCASE);
-    }
-    if config.digit {
-        charset.extend_from_slice(&DIGIT);
-    }
-    if config.symbol {
-        charset.extend_from_slice(&SYMBOL);
-    }
-
-    if charset.is_empty() {
-        return Err("Charset empty".into());
-    }
-
-    Ok(charset)
-}
+mod charset;
+use charset::fill_charset;
 
 fn get_entropy(config: &PasswordConfig, charset: &Vec<char>) -> f64 {
     
@@ -84,7 +41,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     if config.length as usize > charset.len() {
-        return Err("Warning : Charset is too small to guarantee no duplicated char".into());
+        return Err("Error : Charset is too small to guarantee no duplicated char".into());
     }
 
     let password = generation(&charset, config.length, config.duplicate);
@@ -99,9 +56,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
     //Todo : 
-    //- parsing TOML : serde
     //- flag json pour la sortie
-    //- cli : debug entropy json
     //- tests unitaires ?
     //
     //- audit de bruteforce 
